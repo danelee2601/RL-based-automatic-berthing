8a62d07f31541de829874bbbbc2fe0b66fac5ec3
Daesoo Lee <34414758+danelee2601@users.noreply.github.com>
2020-09-21 14:37:33 +0900

    Update load.py

--- utils/load.py
+++ utils/load.py
@@ -1,6 +1,6 @@
 import os
 
-from stable_baselines.common.vec_env import SubprocVecEnv, VecNormalize
+from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv, VecNormalize
 from stable_baselines import PPO2
 
 from .declare_berthing_env import decalre_Berthing_env
@@ -20,7 +20,8 @@ def load_trained_env_n_model(loading_section_num=12):
     # set env
     Berthing_env = decalre_Berthing_env()
     n_cpu = 1  
-    loaded_env = SubprocVecEnv([lambda: CustomEnv(Berthing_env) for i in range(n_cpu)])  # n_cpu=1 during test
+    #oaded_env = SubprocVecEnv([lambda: CustomEnv(Berthing_env) for i in range(n_cpu)])  # n_cpu=1 during test
+    loaded_env = DummyVecEnv([lambda: CustomEnv(Berthing_env)])
     loaded_env = VecNormalize(loaded_env, norm_obs=False, norm_reward=False)  # to load VecNormalize object, norm_obs and norm_reward must be False
     loaded_env = loaded_env.load(save_dirname_env2, loaded_env)
 
